698cdf6b3124bc7978e6d32b059100c331073bf1
jaswanthmagure <jaswanth@maguresoftwares.com>
2020-09-17 11:38:49 +0530

    fix veiw controller file

---
+++ calc-ios/calc-ios/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  calc-ios
+//
+//  Created by jaswanth on 17/09/20.
+//  Copyright © 2020 jaswanth. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Second view"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
